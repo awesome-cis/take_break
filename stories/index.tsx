68bcde9eacf4430928e12dff53063de0331e17bb
oyeahhh <oyeahhh@naver.com>
2019-05-15 09:27:35 +0900

    yarn add @storybook/addon-knobs

--- stories/index.tsx
+++ stories/index.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 import { Button } from "@storybook/react/demo";
 
-storiesOf("Button", module)
+const stories = storiesOf("Storybook Knobs", module);
+
+stories.addDecorator(withKnobs);
+
+stories
   .add("with text", () => <Button>Hello Button</Button>)
   .add("with emoji", () => (
     <Button>
@@ -11,3 +16,11 @@ storiesOf("Button", module)
       </span>
     </Button>
   ));
+
+stories.add("as dynamic variables", () => {
+  const name = text("Name", "Arunoda Susiripala");
+  const age = number("Age", 89);
+
+  const content = `I am ${name} and I'm ${age} years old.`;
+  return <div>{content}</div>;
+});
