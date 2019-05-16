43a5a13c2064528be7a5db96013d5094ec87a116
oyeahhh <oyeahhh@naver.com>
2019-05-16 23:48:54 +0900

    refact: add @storybook/addon-backgrounds

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,6 +1,12 @@
 import { configure, addParameters } from "@storybook/react";
 
-addParameters({ viewport: { defaultViewport: "iphonex" } });
+addParameters({
+  viewport: { defaultViewport: "iphonex" },
+  backgrounds: [
+    { name: "twitter", value: "#00aced", default: true },
+    { name: "facebook", value: "#3b5998" }
+  ]
+});
 
 function loadStories() {
   // automatically import all files ending in *.tsx
