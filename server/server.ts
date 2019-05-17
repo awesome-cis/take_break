214413b7e774fa2c48889f81625e6355b4d44b3c
letsget23 <letsget23@gmail.com>
2019-05-17 17:23:30 +0900

    feat: Add mock apis

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,67 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server: Express.Application = Express();
 
-  server.get('*', (req: Express.Request, res: Express.Response) => {
+  // POST /register
+  server.post('/register', (_req, res) => {
+    res.json({
+      user: {
+        id: 1,
+        name: 'Mock User',
+        nickname: 'Mocked Nickname',
+        email: 'mock@test.com',
+        slug: 'mocking'
+      }
+    });
+  });
+
+  // POST /login
+  server.post('/login', (_req, res) => {
+    res.json({
+      user: {
+        id: 1,
+        name: 'Mock User',
+        nickname: 'Mocked Nickname',
+        email: 'mock@test.com',
+        slug: 'mocking'
+      }
+    });
+  });
+
+  // GET /users/:id
+  server.get('/users/:id', (_req, res) => {
+    res.json({
+      user: {
+        id: 1,
+        name: 'Mock User',
+        nickname: 'Mocked Nickname',
+        email: 'mock@test.com',
+        slug: 'mocking'
+      }
+    });
+  });
+
+  // PUT /users/:id
+  server.put('/users/:id', (_req, res) => {
+    res.json({
+      user: {
+        id: 1,
+        name: 'Mock User',
+        nickname: 'Mocked Nickname',
+        email: 'mock@test.com',
+        slug: 'mocking'
+      }
+    });
+  });
+
+  // GET /summary
+  server.get('/summary', (_req, res) => {
+    res.json({
+      remain: 9999
+    });
+  });
+
+  // ...rest
+  server.get('*', (req, res) => {
     return handle(req, res);
   });
 
