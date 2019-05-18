5f944f57b2cf65220c68536c2f2760184b02e8ea
JinYoung Ryu <oyeahhh@naver.com>
2019-05-18 16:10:04 +0900

    feat: yarn add antd  (#6)
    * refact: yarn add antd
    
    * refact: add Head and buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
+import Head from "next/Head";
+import { Button } from "antd";
+
 const Index = () => {
   const helloText: string = "Hello Next.js, Hello Typescript";
   return (
-    <div>
-      <p>{helloText}</p>
-    </div>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.18.1/antd.min.css"
+        />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/antd/3.18.1/antd.min.js" />
+      </Head>
+      <div>
+        <p>{helloText}</p>
+        <Button type="primary">Primary</Button>
+        <Button>Default</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="danger">Danger</Button>
+        <Button type="link">Link</Button>
+      </div>
+    </>
   );
 };
 
