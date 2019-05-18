cab45ad82f7eda28a06560adcb491f255284877a
JinYoung Ryu <oyeahhh@naver.com>
2019-05-18 16:45:17 +0900

    feat: setting storybook (#5)
    * yarn add -D @babel/core @storybook/react
    
    * setting config.js, create stories/index.js
    
    * yarn add typescript, @types
    
    * setting typescript
    
    * setting storybook typescript
    
    * change js to ts
    
    * yarn add @storybook/addon-knobs
    
    * refact: add addon-viewport
    
    * refact: add @storybook/addon-backgrounds

---
+++ .storybook/config.ts
@@ -0,0 +1,18 @@
+import { configure, addParameters } from "@storybook/react";
+
+addParameters({
+  viewport: { defaultViewport: "iphonex" },
+  backgrounds: [
+    { name: "twitter", value: "#00aced", default: true },
+    { name: "facebook", value: "#3b5998" }
+  ]
+});
+
+function loadStories() {
+  // automatically import all files ending in *.tsx
+  const req = require.context("../stories", true, /.tsx$/);
+
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
---
+++ stories/index.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, text, number } from "@storybook/addon-knobs";
+import { Button } from "@storybook/react/demo";
+
+const stories = storiesOf("Storybook Knobs", module);
+
+stories.addDecorator(withKnobs);
+
+stories
+  .add("with text", () => <Button>Hello Button</Button>)
+  .add("with emoji", () => (
+    <Button>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+
+stories.add("as dynamic variables", () => {
+  const name = text("Name", "Arunoda Susiripala");
+  const age = number("Age", 89);
+
+  const content = `I am ${name} and I'm ${age} years old.`;
+  return <div>{content}</div>;
+});
