3a1da1444d8c8b644aa59163579a2db1f586a625
oyeahhh <oyeahhh@naver.com>
2019-05-20 00:23:13 +0900

    feat: add next-less

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const withTypescript = require("@zeit/next-typescript");
 const withSass = require("@zeit/next-sass");
+const withLess = require("@zeit/next-less");
 
-module.exports = withTypescript(withSass());
+module.exports = withTypescript(
+  withLess(
+    withSass({
+      lessLoaderOptions: {
+        javascriptEnabled: true
+      }
+    })
+  )
+);
