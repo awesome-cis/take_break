996b3f47ac25c59c30c32735e8daa3ed00bf1886
oyeahhh <oyeahhh@naver.com>
2019-05-20 00:23:33 +0900

    style: edit global style (font, color)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import "../src/styles/ant.less";
 import Head from "next/Head";
 import { Button } from "antd";
 
@@ -6,6 +7,10 @@ const Index = () => {
   return (
     <>
       <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat|Nanum+Gothic&display=swap"
+          rel="stylesheet"
+        />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.18.1/antd.min.css"
