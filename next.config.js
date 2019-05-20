8186e4f2441203272752e9c4b48d447a9fae90d7
oyeahhh <oyeahhh@naver.com>
2019-05-20 21:28:39 +0900

    refact: settting absolute-imports

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-const withTypescript = require("@zeit/next-typescript");
-const withSass = require("@zeit/next-sass");
+const path = require('path');
+const withTypescript = require('@zeit/next-typescript');
+const withSass = require('@zeit/next-sass');
 
-module.exports = withTypescript(withSass());
+module.exports = withTypescript(
+  withSass({
+    webpack(config, options) {
+      config.resolve.alias['components'] = path.join(
+        __dirname,
+        'src/components'
+      );
+      return config;
+    }
+  })
+);
