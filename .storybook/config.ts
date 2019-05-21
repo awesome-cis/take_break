54306b3a3f5d939dbcadb5b5283e0e82e7e67491
oyeahhh <oyeahhh@naver.com>
2019-05-22 00:44:07 +0900

    refact: save singleQuote options

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,16 +1,16 @@
-import { configure, addParameters } from "@storybook/react";
+import { configure, addParameters } from '@storybook/react';
 
 addParameters({
-  viewport: { defaultViewport: "iphonex" },
+  viewport: { defaultViewport: 'iphonex' },
   backgrounds: [
-    { name: "twitter", value: "#00aced", default: true },
-    { name: "facebook", value: "#3b5998" }
+    { name: 'twitter', value: '#00aced', default: true },
+    { name: 'facebook', value: '#3b5998' }
   ]
 });
 
 function loadStories() {
   // automatically import all files ending in *.tsx
-  const req = require.context("../stories", true, /.tsx$/);
+  const req = require.context('../stories', true, /.tsx$/);
 
   req.keys().forEach(filename => req(filename));
 }
--- stories/index.tsx
+++ stories/index.tsx
@@ -1,25 +1,24 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import { withKnobs, text, number } from "@storybook/addon-knobs";
-import { Button } from "@storybook/react/demo";
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text, number } from '@storybook/addon-knobs';
 
-const stories = storiesOf("Storybook Knobs", module);
+const stories = storiesOf('Storybook Knobs', module);
 
 stories.addDecorator(withKnobs);
 
 stories
-  .add("with text", () => <Button>Hello Button</Button>)
-  .add("with emoji", () => (
-    <Button>
+  .add('with text', () => <button>Hello Button</button>)
+  .add('with emoji', () => (
+    <button>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
-    </Button>
+    </button>
   ));
 
-stories.add("as dynamic variables", () => {
-  const name = text("Name", "Arunoda Susiripala");
-  const age = number("Age", 89);
+stories.add('as dynamic variables', () => {
+  const name = text('Name', 'Arunoda Susiripala');
+  const age = number('Age', 89);
 
   const content = `I am ${name} and I'm ${age} years old.`;
   return <div>{content}</div>;
