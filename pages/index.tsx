4e9d830c1086dc9175960a378d198441bdb1213b
oyeahhh <oyeahhh@naver.com>
2019-05-29 20:53:06 +0900

    refact: reset index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,5 @@
-import 'styles/index.scss';
-import Login from 'components/Login';
-import { Typography, Row, Col } from 'antd';
-
-const { Title } = Typography;
-
 const Index: React.FC = () => {
-  return (
-    <Row
-      type="flex"
-      align={'middle'}
-      justify={'center'}
-      className={'container'}
-    >
-      <Col md={{ span: 10 }} xs={{ span: 16 }}>
-        <Title>Take Break</Title>
-        <Login />
-      </Col>
-    </Row>
-  );
+  return <div>Index</div>;
 };
 
 export default Index;
