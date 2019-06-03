56ba046e14edbfed3fe2c98b5e9ef79d23fc1d90
letsget23 <letsget23@gmail.com>
2019-06-03 12:08:22 +0900

    feat: update callback example

--- client_example/src/components/AuthGithub.js
+++ client_example/src/components/AuthGithub.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 const CLIENT_ID = '33b55a9eb249d40cf9da';
 
+/**
+ * @description [인증 진행] 버튼을 눌리면 github.com에 로그인된 이후, GitHub Apps 설정에 정의된 콜백으로 자동으로 Redirect 합니다.
+ */
 export default class AuthGithub extends Component {
   render() {
     return (
--- client_example/src/components/AuthGithubCallback.js
+++ client_example/src/components/AuthGithubCallback.js
@@ -4,13 +4,19 @@ import { withRouter } from 'react-router';
 const queryString = require('query-string');
 const superagent = require('superagent');
 
+/**
+ * @description GitHub로 부터 인증된 이후 받은 code를 서버로 전송해 로그인을 처리함
+ */
 class AuthGithubCallback extends Component {
+  state = {
+    res: null
+  };
+
   componentDidMount() {
     const parsed = queryString.parse(this.props.location.search);
-    console.log(parsed.code);
-
     const code = parsed.code;
 
+    // server에 github 로그인 or 회원가입 요청
     superagent
       .post('http://localhost:8080/auth/github')
       .send({
@@ -19,11 +25,19 @@ class AuthGithubCallback extends Component {
       .set('Content-Type', 'application/json')
       .set('Accept', 'application/json')
       .end((err, res) => {
-        console.log(res);
+        this.setState({
+          res: res.body
+        });
+        console.log(res.body);
       });
   }
+
   render() {
-    return <div className="AuthGithubCallback" />;
+    return (
+      <div className="AuthGithubCallback">
+        <pre>{JSON.stringify(this.state.res, null, 2)}</pre>
+      </div>
+    );
   }
 }
 
