70993c5f1310d2d30a27a7e33d1f9229d11c047a
oyeahhh <oyeahhh@naver.com>
2019-06-12 23:56:05 +0900

    refact: edit pages/index for test

--- take_break_client/pages/index.tsx
+++ take_break_client/pages/index.tsx
@@ -1,5 +1,28 @@
-const Index: React.FC = () => {
+import { NextFunctionComponent } from 'next';
+
+import agent from '../agent';
+
+const Index: NextFunctionComponent = props => {
+  // TODO 확인 후 console.log 제거
+  console.log('props', props);
   return <div>Index</div>;
+};
+
+Index.getInitialProps = async function() {
+  const res = await agent.post('/auth/register', {
+    name: 'ryu',
+    email: 'oyeahhh@naver.com',
+    password: 'oyeahhh'
+  });
+
+  const data = await res.json();
+
+  // TODO 확인 후 console.log 제거
+  console.log('data', data);
+
+  return {
+    resData: data
+  };
 };
 
 export default Index;
